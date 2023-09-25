6b061267833d69b0c97ee5a09735c8b0e6967b7d
korolevichlisa <korolevich.liza@gmail.com>
2023-09-25 14:41:54 +0300

    change design

--- testdb/views.py
+++ testdb/views.py
@@ -1,34 +1,15 @@
-# from django.shortcuts import render
-# from django.views.generic import ListView
-# from django import http
-# from django.http import HttpResponse
-# from django.shortcuts import render
-# from testdb.models import College, New, Library_funds, Room, Library_servic, Exhibition, Coworkers
 from django import http
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from testdb.models import College, New, Library_funds, Library_room, Library_servic, Exhibition, Coworkers, Photos
 
-# # from .models import Colleges, News
-# # Create your views here.
 
-# # def tfk(request):
-# #     return render(request, "tfk.html")
-# class tfk(ListView):
-#     news = New.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж').order_by('-published_date')[:3:-1] 
-#     coworker = Coworkers.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж').values()
-#     fund = Library_funds.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж').values()
-#     room = Room.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж').values()
-#     servivic = Library_servic.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж').values()
-#     exhibition = Exhibition.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж').values()
-#     # return render(ListView, "tfk.html",{'news':news, 'coworker':coworker, 'fund':fund, 'room':room, 'servic':servivic, 'exhibition':exhibition})
 def home(request):
-    # coworker = Coworkers.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж')
     return render(request, "index.html")
 
 def tfk(request):
     news = New.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж').order_by('-published_date')[:3:-1] 
-    coworker = Coworkers.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж')
+    coworker = Coworkers.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж').order_by('-published_date')[::-1]
     fund = Library_funds.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж')
     room = Library_room.objects.filter(id_college__name_college = 'Одеський технічний фаховий коледж')
     print(room)
@@ -39,7 +20,7 @@ def tfk(request):
     return render(request, "tfk.html",{'news':news, 'coworker':coworker, 'fund':fund, 'room':room, 'photos':photos, 'servic':servivic, 'exhibition':exhibition})
 
 def mt(request):
-    news = New.objects.filter(id_college__name_college = 'Механіко-технологічний фаховий коледж').order_by('-published_date')[:5:-1] 
+    news = New.objects.filter(id_college__name_college = 'Механіко-технологічний фаховий коледж').order_by('-published_date')[:3:-1] 
     coworker = Coworkers.objects.filter(id_college__name_college = 'Механіко-технологічний фаховий коледж')
     fund = Library_funds.objects.filter(id_college__name_college = 'Механіко-технологічний фаховий коледж')
     room = Library_room.objects.filter(id_college__name_college = 'Механіко-технологічний фаховий коледж')
